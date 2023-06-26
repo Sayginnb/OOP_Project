1453ef9e1617962c35bf4f4bcd0c3bfcf6176ec7
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-06-26 12:33:33 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Address
+    {
+        public int AddressID { get; set; }
+        public int Description1 { get; set; }
+        public int Description2 { get; set; }
+        public int Description3 { get; set; }
+        public int Description4 { get; set; }
+        public string Mapinfo { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Team.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Team
+    {
+        public int TeamId { get; set; }
+        public string PersonName { get; set; }
+        public int Title { get; set; }
+        public int ImageUrl { get; set; }
+        public int FacebookUrl { get; set; }
+        public int InstagramUrl { get; set; }
+        public int WebSiteUrl { get; set; }
+        public int TwitterUrl { get; set; }
+    }
+}
