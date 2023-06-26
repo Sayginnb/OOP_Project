ce53f41015e6687d78482b931955443b6ecf8670
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-06-26 12:54:58 +0300

    Context

--- EntityLayer/Concrete/Announcement.cs
+++ EntityLayer/Concrete/Announcement.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace EntityLayer.Concrete
 {
-    public class News
+    public class Announcement
     {
-        public int NewsID { get; set; }    
+        public int AnnouncementID { get; set; }    
         public string Title { get; set; }    
         public string Description { get; set; }    
         public DateTime Date { get; set; }    
