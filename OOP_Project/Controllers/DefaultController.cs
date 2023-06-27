f283c43862d201ec51fb15d5fe59157121b599f4
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-06-27 14:31:19 +0300

    Manager new list

---
+++ OOP_Project/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OOP_Project.Controllers
+{
+    public class DefaultController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+        public IActionResult Index()
+        {
+            var values = serviceManager.GetListAll();
+            return View(values);
+        }
+    }
+}
