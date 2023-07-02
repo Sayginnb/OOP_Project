83459fb501b59fadbdbe33e6ab4ceb373e088267
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-07-02 12:46:44 +0300

    Add image

--- BusinessLayer/ValidationRules/ImageValidator.cs
+++ BusinessLayer/ValidationRules/ImageValidator.cs
@@ -15,7 +15,7 @@ namespace BusinessLayer.ValidationRules
             RuleFor(x => x.Title).NotEmpty().WithMessage("Görsel başlığı boş geçilemez!");
             RuleFor(x => x.Description).NotEmpty().WithMessage("Görsel açıklaması boş geçilemez!");
             RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Görsel yolu boş geçilemez!");
-            RuleFor(x => x.Title).MaximumLength(20).WithMessage("Lütfen en fazla 20 karakter veri girişi yapınız");
+            RuleFor(x => x.Title).MaximumLength(30).WithMessage("Lütfen en fazla 30 karakter veri girişi yapınız");
             RuleFor(x => x.Title).MinimumLength(8).WithMessage("Lütfen en az 8 karakter veri girişi yapınız");
             RuleFor(x => x.Description).MaximumLength(50).WithMessage("Lütfen en fazla 50 karakter veri girişi yapınız");
             RuleFor(x => x.Description).MinimumLength(20).WithMessage("Lütfen en az 20 karakter veri girişi yapınız");
