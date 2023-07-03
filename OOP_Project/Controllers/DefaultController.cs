a04c96a0e68b3eeaa797166b5e9a5f4fffa16d8f
Burşah SAYGIN <b.sayginnn@gmail.com>
2023-07-03 12:50:17 +0300

    User page

--- OOP_Project/Controllers/DefaultController.cs
+++ OOP_Project/Controllers/DefaultController.cs
@@ -7,11 +7,9 @@ namespace OOP_Project.Controllers
 {
     public class DefaultController : Controller
     {
-        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
         public IActionResult Index()
         {
-            var values = serviceManager.GetListAll();
-            return View(values);
+            return View();
         }
     }
 }
